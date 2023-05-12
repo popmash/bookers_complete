c8377e046750ceeca6f849c09338299f1abe50c8
popmash <131745061+popmash@users.noreply.github.com>
2023-05-12 10:41:32 +0000

    Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,22 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+      
+      
 
   validates :name, uniqueness: true, length: { in: 2..20}
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
     resources :book_comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
